80c092aad1a12b04b8fe9bdfd157b0c3088fe8bd
Jonathan Wu <jwu20@stuy.edu>
2020-12-06 23:11:41 -0500

    started range class and range method

---
+++ Range.java
@@ -0,0 +1,10 @@
+import java.util.NoSuchElementException;
+
+public class Range implements IntegerSequence {
+  private int start, end, current;
+
+  public Range(int start, int end) {
+    this.start = start;
+    this.end = end;
+  }
+}
