0a06d38e53afd32c39ea7bc0af2d00d49509b0cf
Jonathan Wu <jwu20@stuy.edu>
2020-12-08 11:44:52 -0500

    fixed bugs in ArraySequence

--- ArraySequence.java
+++ ArraySequence.java
@@ -5,19 +5,25 @@ public class ArraySequence implements IntegerSequence {
   private int []data;
 
   public ArraySequence(int [] other) {
+    currentIndex = 0;
+    data = new int[other.length];
     for (int i = 0; i < other.length; i++) {
       data[i] = other[i];
     }
-    currentIndex = 0;
   }
 
   public boolean hasNext() {
-    return (currentIndex <= data.length) ;
+    return (currentIndex < data.length);
   }
 
   public int next() {
-    currentIndex++;
-    return data[currentIndex-1];
+    if (hasNext()) {
+      currentIndex++;
+      return data[currentIndex-1];
+    } else {
+      currentIndex++;
+      throw new NoSuchElementException((currentIndex - 1) + " is not within the range of this sequence.");
+    }
   }
 
   public int length() {
