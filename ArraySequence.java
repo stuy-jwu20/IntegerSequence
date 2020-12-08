aeab8404b970e7140dffdf5f0eb3f0f34534ef1e
Jonathan Wu <jwu20@stuy.edu>
2020-12-08 11:36:45 -0500

    added ArraySequence file

---
+++ ArraySequence.java
@@ -0,0 +1,30 @@
+import java.util.NoSuchElementException;
+
+public class ArraySequence implements IntegerSequence {
+  private int currentIndex;
+  private int []data;
+
+  public ArraySequence(int [] other) {
+    for (int i = 0; i < other.length; i++) {
+      data[i] = other[i];
+    }
+    currentIndex = 0;
+  }
+
+  public boolean hasNext() {
+    return (currentIndex <= data.length) ;
+  }
+
+  public int next() {
+    currentIndex++;
+    return data[currentIndex-1];
+  }
+
+  public int length() {
+    return data.length;
+  }
+
+  public void reset() {
+    currentIndex = 0;
+  }
+}
